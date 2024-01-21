df6d818d8f6be987c681d69e39676c91b77fef26
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-21 11:04:15 +0100

    100-shell_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -58,7 +58,7 @@ int partition(int *arr, int low, int high, int size)
  */
 void quickSort_helper(int *arr, int low, int high, int size)
 {
-	if (high - low > 0)
+	if (low < high)
 	{
 		int pi = partition(arr, low, high, size); /*Partition the array*/
 
